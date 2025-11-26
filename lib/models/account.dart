ff265d360565e929a04c0657a429920c1f3a4ec0
ragielly <ragielly.19@gmail.com>
2025-11-26 14:58:03 -0300

    adicionando classe

---
+++ lib/models/account.dart
@@ -0,0 +1,32 @@
+class Account {
+  String id;
+  String name;
+  String lastName;
+  double balance;
+
+  Account({
+    required this.id,
+    required this.name,
+    required this.lastName,
+    required this.balance,
+  });
+//map converter para account
+  factory Account.fromMap(Map<String, dynamic> map) {
+    return Account(
+      id: map["id"],
+      name: map["name"],
+      lastName: map["lastName"],
+      balance: map["balance"],
+    );
+  }
+
+//account converter para map
+  Map<String, dynamic> toMap(){
+    return<String, dynamic>{
+      "id" : id,
+      "name" : name,
+      "lastName" : lastName,
+      "balance" : balance,
+    };
+  }
+}
