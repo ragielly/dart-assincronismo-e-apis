cb1898cd9fb74c15f601fd2e2c00a4d4b8b2a535
ragielly <ragielly.19@gmail.com>
2025-11-26 13:42:06 -0300

    adicionando tratamento de url ao sistema

--- bin/main.dart
+++ bin/main.dart
@@ -1,47 +1,67 @@
+import 'dart:async';
 
+import 'package:dart_asynchronous/api_key.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+
+StreamController<String> streamController = StreamController<String>();
 void main() {
-  // print('ola mundo');
-  // requestData();
-  // requestDataAsync();
+  StreamSubscription streamSubscription = streamController.stream.listen((
+    String info,
+  ) {
+    print(info);
+  });
+
+  requestData();
+  requestDataAsync();
   sendDataAsync({
     "id": "NEW001",
     "name": "Flutter",
     "lastName": "Dart",
     "balance": 5000,
   });
-  }
+}
 
 void requestData() {
-  String url = "https://gist.githubusercontent.com/ragielly/2e6a49b0e6403d1188084e027ec1c975/raw/1209e5a4a12b8b55697f39d414ad8e43d81ba2cf/gistfile1.txt";
-  Future<Response> futureResponse =  get(Uri.parse(url));
-  print(futureResponse);
-  futureResponse.then((Response response){
-    // print(response);
-    // print(response.body);
-    List<dynamic> listAccount =  json.decode(response.body);
-    Map<String, dynamic> mapCarla = listAccount.firstWhere(
-      (element) => element["name"] == "Carla"
-      );
-      print(mapCarla["balance"]);
-  },
-  );
+  String url =
+      "https://gist.githubusercontent.com/ragielly/2e6a49b0e6403d1188084e027ec1c975/raw/a48ea2fd9d16bd55b9bfd9268b208e227a43f8c6/accounts.json";
+  Future<Response> futureResponse = get(Uri.parse(url));
+
+  futureResponse.then((Response response) {
+    streamController.add(
+      " ${DateTime.now()} | Requisição Leitura (usando then) ",
+    );
+  });
 }
 
-Future<List<dynamic>> requestDataAsync () async {
-  String url = "https://gist.githubusercontent.com/ragielly/2e6a49b0e6403d1188084e027ec1c975/raw/1209e5a4a12b8b55697f39d414ad8e43d81ba2cf/gistfile1.txt";
+Future<List<dynamic>> requestDataAsync() async {
+  String url =
+      "https://gist.githubusercontent.com/ragielly/2e6a49b0e6403d1188084e027ec1c975/raw/a48ea2fd9d16bd55b9bfd9268b208e227a43f8c6/accounts.json";
   Response response = await get(Uri.parse(url));
+  streamController.add("${DateTime.now()} | Requisição Leitura }");
   return json.decode(response.body);
-
 }
-void sendDataAsync(Map<String,dynamic> mapAccount) async {
+
+void sendDataAsync(Map<String, dynamic> mapAccount) async {
   List<dynamic> listAccounts = await requestDataAsync();
   listAccounts.add(mapAccount);
-  String content = json.encode(listAccounts) ;
+  String content = json.encode(listAccounts);
 
-  String url = "https://gist.githubusercontent.com/ragielly/2e6a49b0e6403d1188084e027ec1c975/raw/1209e5a4a12b8b55697f39d414ad8e43d81ba2cf/gistfile1.txt";
-  Response response = await post(Uri.parse(url), body: content);
-  print(response.statusCode);
+  String url = "https://api.github.com/gists/2e6a49b0e6403d1188084e027ec1c975";
+  Response response = await post(
+    Uri.parse(url),
+    headers: {"Authorization": "Bearer $gitHubApiKey"},
+    body: json.encode({
+      "description": "accounts.json",
+      "public": true,
+      "files": {
+        "accounts.json": {"content": content},
+      },
+    }),
+  );
+  if(response.statusCode.toString()[0] == "2" ){
+    streamController.add("${DateTime.now()} | Requisição adição bem sucedida ${mapAccount["name"]} .");
+  }else{
+    streamController.add("${DateTime.now()} | Requisição adição falhou .");
+  }
 }
-
